5b73ed0a939f185675939a217a1550a3a05c6bcb
Andrei Mihai <user.andrei@yahoo.com>
2025-06-01 09:04:35 +0300

    Add simple hello world and flake8

---
+++ python/ex2.py
@@ -0,0 +1,11 @@
+# Declare different variables types and print them
+
+age = 23  # Integer
+cost = 12.3  # Float
+name = "Popescu Ioana"  # String
+is_valid = True  # Boolean
+
+print("Age is", age)
+print(f"Cost is {cost}")
+print(f"Name is {name}")
+print(f"is_valid {is_valid}")
