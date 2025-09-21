ac3fcb205697c88cda817d1c45e2a7a3a1263cc8
Andrei Mihai <user.andrei@yahoo.com>
2025-09-21 11:46:23 +0300

    Add ex 11 jenkins

--- jenkins/start-jenkins-docker-compose.sh
+++ jenkins/start-jenkins-docker-compose.sh
@@ -5,6 +5,10 @@ echo "Start Jenkins In folder: $JENKINS_HOME"
 if [ ! -d "$JENKINS_HOME" ]; then
 	echo "Folder $JENKINS_HOME is missing. Try to create it."
 	mkdir $JENKINS_HOME
+	if [ ! -d "$JENKINS_HOME" ]; then
+		echo "Cannot create Jenkins home"
+		exit 1
+	fi
 fi
 
 docker compose up 
